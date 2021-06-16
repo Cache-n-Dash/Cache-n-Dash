470c7ffa96717ae55fce393a8a2efeeda5b3c9d8
stacefacee <81988886+stacefacee@users.noreply.github.com>
2021-06-16 14:34:27 -0700

    commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,34 @@
+import {Link} from 'react-router-dom';
+import {useState} from 'react';
+
+
+
+function Header() {
+  const [menu, setMenu] = useState(false)
+
+
+  const menuClick = () => {
+    setMenu(!menu)
+  }
+
+
+  return (
+    <div className="header">
+      <h1>Cache-N-Dash</h1>
+      <button className= "menu-button" onClick= {menuClick}>Menu</button>
+      {menu && 
+        <div className="menu">
+      <li><Link to='/'>Home</Link> </li>
+        <li><Link to='/search'>Search</Link> </li>
+        <li><Link to='/map'>Race</Link> </li>
+        <li><Link to='/leaderboard'>Leaderboard</Link> </li>
+        
+      </div> }
+      
+
+
+    </div>
+
+
+  )
+}
