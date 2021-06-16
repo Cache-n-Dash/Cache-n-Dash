323d0e5ba9c3842e139596e915a54015ea8ee21e
Gavin Young <younnit@gmail.com>
2021-06-16 16:03:23 -0600

    Worked on map.js

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,12 +1,49 @@
-import React from 'react'
-import './Map.css'
+import React, { useCallback } from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import "./Map.css";
+// import mapStyles from "./mapStyles";
+
+const libraries = ["places"];
+
+const mapContainerStyle = {
+    width: "100vw",
+    height: "99vh",
+  };
+
+const center = {
+  lat: 37,
+  lng: -100,
+};
+
+const options = {
+  zoomControl: true,
+  disableDefaultUI: true,
+//   styles: mapStyles
+};
 
 function Map() {
-    return (
-        <div>
-            Map
-        </div>
-    )
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+
+  const onLoad = useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={mapContainerStyle}
+      center={center}
+      options={options}
+      zoom={8}
+      onLoad={onLoad}
+    >
+      <></>
+    </GoogleMap>
+) : <></>
 }
 
-export default Map
+export default Map;
