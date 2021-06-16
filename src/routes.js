eee74186f991c664302173da17bb0b0244e4293f
Gavin Young <younnit@gmail.com>
2021-06-16 15:04:23 -0600

    Created contact route and contact files

--- src/routes.js
+++ src/routes.js
@@ -4,18 +4,20 @@ import Auth from './components/Auth/Auth'
 import UserDash from './components/UserDash/UserDash'
 import LeaderBoard from './components/LeaderBoard/LeaderBoard'
 import Map from './components/Map/Map'
-import React from "react";
 import Search from './components/Search/Search'
 import Admin from './components/Admin/Admin'
+import Contact from './components/Contact/Contact'
+import React from "react";
 
 export default (
   <Switch>
     <Route exact path="/admin" component={Admin} />
-    <Route exact path="/auth" component={Auth} />
-    <Route exact path="/" component={Home} />
-    <Route exact path="/leaderboard" component={LeaderBoard} />
-    <Route exact path="/map" component={Map} />
-    <Route exact path="/search" component={Search} />
-    <Route exact path="/userdash" component={UserDash} />
+    <Route path="/auth" component={Auth} />
+    <Route path="/" component={Home} />
+    <Route path="/leaderboard" component={LeaderBoard} />
+    <Route path="/map" component={Map} />
+    <Route path="/search" component={Search} />
+    <Route path="/userdash" component={UserDash} />
+    <Route path='/contact' component={Contact} />
   </Switch>
 );
