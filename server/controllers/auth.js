5b6c06647583281c2ac15e7068da18599a731efa
stacefacee <81988886+stacefacee@users.noreply.github.com>
2021-06-16 09:09:05 -0700

    commit

---
+++ server/controllers/auth.js
@@ -0,0 +1,47 @@
+const bcrypt = require('bcryptjs');
+module.exports = {
+  register: async (req, res) => {
+    const db = req.app.get('db')
+    const {email, password} = req.body
+    const [checkUser] = await db.users.get_user_by_email(email)
+    if(checkUser){
+      return res.status(409).send("Email is already registered.")
+    }
+    const salt = bcrypt.genSaltSync(10)
+    const hash = bcrypt.hashSync(password, salt)
+    const [user] = await db.users.create_user(email, hash)
+    delete user.password
+    req.session.user = user
+    return res.status(200).send(req.session.user)
+  },
+
+  login: async (req,res) => {
+    const db = req.app.get('db')
+    const {email, password} = req.body
+    const [user] = await db.users.get_user_by_email(email)
+    if (!user){
+      return res.status(401).send("Email not found.")
+    }
+    const isAuthenticated = bcrypt.compareSync(password, user.password)
+    if(!isAuthenticated){
+      return res.status(403).send("Incorrect Password.")
+    }
+    delete user.password
+    req.session.user = user
+    return res.status(200).send(req.session.user)
+},
+
+  logout: (req, res) => {
+    if(!req.session.user){
+      return res.status(404).send("User not found.")
+    }
+    return res.status(200).send(req.session.user)
+  },
+
+  getUser: (req, res) => {
+    if(!req.session.user){
+      return res.status(404).send ("User not found.")
+    }
+     return res.status(200).send(req.session.user)
+  }
+}
