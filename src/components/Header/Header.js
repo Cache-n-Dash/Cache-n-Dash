c4b6593f432fd1f4f275501b52a9b4c2fac273fe
Gavin Young <younnit@gmail.com>
2021-06-16 12:05:05 -0600

    Created the skeleton for components and Links for the header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div>
+            <nav>
+                <Link to='/'>Home</Link>
+                <Link to='/auth'>Auth</Link>
+                <Link to='/admin'>Admin</Link>
+                <Link to='/leaderboard'>LeaderBoard</Link>
+                <Link to='/map'>Map</Link>
+                <Link to='/search'>Search</Link>
+                <Link to='/userdash'>UserDash</Link>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
