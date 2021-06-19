555f4611bb1825b34fa511eea8806c133d8c9452
jeff-mcc <reffery@gmail.com>
2021-06-19 14:52:35 -0600

    fixed bugs to ensure proper table insertion in geo_courses and geo_course_locs in database

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Home.css'
+import CourseGen from '../CourseGen/CourseGen'
 
 function Home() {
     return (
         <div>
             Home
+            <CourseGen />
         </div>
     )
 }
