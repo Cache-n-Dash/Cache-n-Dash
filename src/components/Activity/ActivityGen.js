4f72e35f46bfea206e9106cd0b6754353c38b09d
jeff-mcc <reffery@gmail.com>
2021-06-19 16:22:30 -0600

    created boilerplate for activity page

---
+++ src/components/Activity/ActivityGen.js
@@ -0,0 +1,13 @@
+// import {useState} from 'react'
+// import axios from 'axios'
+
+const ActivityGen = () => {
+
+    return(
+        <div>
+            test
+        </div>
+    )
+}
+
+export default ActivityGen
