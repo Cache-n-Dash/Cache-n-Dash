201f71728e54413a09ba546aaae00873431b5f6c
jeff-mcc <reffery@gmail.com>
2021-06-19 19:36:57 -0600

    added code to get courses from database with activitygen file

--- src/components/Activity/ActivityGen.js
+++ src/components/Activity/ActivityGen.js
@@ -1,13 +1,44 @@
-// import {useState} from 'react'
-// import axios from 'axios'
+import {useState,useEffect} from 'react'
+import axios from 'axios'
 
 const ActivityGen = () => {
+    const [selected,setSelected] = useState(false)
+    const [courses,setCourses] = useState([])
+
+    useEffect(()=>{
+        axios.get("/courses/all")
+        .then(res=>{
+            // setCourses(res.data)
+            console.log(res.data)
+        }).catch(err=>{
+            console.log(err)
+        })
+    }, []);
+
+    const renderData = () => {
+        if(!selected){
+            return(
+                <div>
+                    all course data to be rendered here
+                </div>
+            )
+        }else{
+            return(
+                <div>
+                    detailed course data to be rendered here for a single course
+                </div>
+            )
+        }
+    }
 
     return(
         <div>
-            test
+            {renderData()}
         </div>
     )
 }
 
 export default ActivityGen
+
+//-- INSERT INTO geo_locations (location_name,latitude,longitude)
+//-- VALUES ('Cemetery1',40.460373,-111.775993),('Cemetery2',40.459141,-111.775734),('Cemetery3',40.460947,-111.774051);
