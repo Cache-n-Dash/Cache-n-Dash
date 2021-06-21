7b78d9601fbab7a3b88ed98f164fa3ca454e7777
jeff-mcc <reffery@gmail.com>
2021-06-21 15:09:33 -0600

    commented edits to Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Home.css'
-import ActivityGen from '../Activity/ActivityGen'
+// import ActivityGen from '../Activity/ActivityGen'
 
 function Home() {
     return (
         <div>
             Home
-            <ActivityGen />
+            {/* <ActivityGen /> */}
         </div>
     )
 }
