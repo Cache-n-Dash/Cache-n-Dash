f4738d001eda337d07987f4aaffb0f6b07e2678a
jeff-mcc <reffery@gmail.com>
2021-06-21 11:39:09 -0600

    basic styling for displaying courses and their locations

--- src/components/Activity/ActivityGen.js
+++ src/components/Activity/ActivityGen.js
@@ -1,9 +1,11 @@
 import {useState,useEffect} from 'react'
 import axios from 'axios'
+import './ActivityGen.css'
 
 const ActivityGen = () => {
     const [selected,setSelected] = useState(false)
     const [courses,setCourses] = useState([])
+    const [crseLocs,setCrseLocs] = useState([])
     // console.log(selected)
 
     useEffect(()=>{
@@ -27,14 +29,27 @@ const ActivityGen = () => {
                 const handleCourseClick = () => {
                     axios.get(`/locations/courses/${crse.course_id}`)
                     .then(res=>{
-                        console.log(res.data)
+                        // console.log(res.data)
+                        setCrseLocs(res.data)
                     }).catch(err=>console.log(err))
                     setSelected(!selected)
                 }
 
                 return(
                     <div key={idx}>
-                        <button onClick={handleCourseClick}>{crse.course_name} | {crse.locations} | {avg}</button>
+                        <button className="courseBtn" onClick={handleCourseClick}><div className="btnDiv numDiv">{idx+1}</div><div className="btnDiv">{crse.course_name}</div><div className="btnDiv">{crse.locations}</div><div className="btnDiv">{avg}</div></button>
+                    </div>
+                )
+            })
+        )
+    }
+
+    const renderCourseLocs = () => {
+        return(
+            crseLocs.map((crse,idx)=>{
+                return(
+                    <div className="locContainer" key={idx}>
+                        <p className="courseLoc numDiv">{crse.location_num}</p><p className="courseLoc">{crse.location_name}</p><p className="courseLoc">{crse.latitude}</p><p className="courseLoc">{crse.longitude}</p><p className="courseLoc">{crse.seg_dist}</p>
                     </div>
                 )
             })
@@ -44,16 +59,17 @@ const ActivityGen = () => {
     const renderData = () => {
         if(!selected){
             return(
-                <div>
-                    <p>Course Name | # of Geolocations | Average Completion Time</p>
+                <div className="containerDiv">
+                    <div className="tableHeaderDiv"><p className="tableHeader crseLayout numDiv">Number</p><p className="tableHeader crseLayout">Course Name</p><p className="tableHeader crseLayout"># of Geolocations</p><p className="tableHeader crseLayout">Avg Completion Time</p></div>
                     {renderCourses()}
                 </div>
             )
         }else{
             return(
-                <div>
-                    <button onClick={()=>setSelected(!selected)}>Go Back</button>
-                    detailed course data to be rendered here for a single course
+                <div className="containerDiv">
+                    <div className="flexBtn"><button onClick={()=>setSelected(!selected)}>Go Back</button></div>
+                    <div className="tableHeaderDiv"><p className="tableHeader locLayout numDiv">Location</p><p className="tableHeader locLayout">Name</p><p className="tableHeader locLayout">Latitude</p><p className="tableHeader locLayout">Longitude</p><p className="tableHeader locLayout">Distance to Next</p></div>
+                    {renderCourseLocs()}
                 </div>
             )
         }
