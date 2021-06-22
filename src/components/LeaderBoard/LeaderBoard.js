84a630506391bf525d3cbe80bb59c6a584a17f01
jeff-mcc <reffery@gmail.com>
2021-06-22 16:43:58 -0600

    began work building out the leaderboard

--- src/components/LeaderBoard/LeaderBoard.js
+++ src/components/LeaderBoard/LeaderBoard.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
 import './LeaderBoard.css'
+import axios from 'axios'
 
 function LeaderBoard() {
+    const [course,setCourse] = useState({})
+    const [courseID,setCourseID] = useState(14)
+    const [courseLeaders,setCourseLeaders] = useState([])
+    // const [segmentLocs,setSegmentLocs] = useState([])
+
+    useEffect(()=>{
+        axios.get(`/courses/${courseID}`)
+        .then(res=>{
+            setCourse(res.data[0])
+            // console.log(res.data[0])
+        }).catch(err=>console.log(err))
+        axios.get(`/leaderboard/${courseID}`)
+        .then(res=>{
+            setCourseLeaders(res.data)
+        }).catch(err=>console.log(err))
+    },[courseID])
+
     return (
         <div>
             Leaderboard
