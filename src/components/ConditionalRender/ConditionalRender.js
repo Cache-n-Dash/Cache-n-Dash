803ff0b5b9dbef040e1b23637205af4a2a421bc8
jeff-mcc <reffery@gmail.com>
2021-06-26 18:14:17 -0600

    updated conditional rendering of locations

--- src/components/ConditionalRender/ConditionalRender.js
+++ src/components/ConditionalRender/ConditionalRender.js
@@ -33,10 +33,38 @@ function ConditionalRender(props) {
     activator()
   }
 
+  const renderList = () => {
+    if(!activated){
+      return(
+        render.map((location) => {
+          if(location.latitude <= props.north && location.latitude >= props.south && location.longitude >= props.west && location.longitude <= props.east){
+            return (
+              <div className="location-info" key={location.location_id}>
+                {location.location_name !== null && (
+                  <h4
+                    onClick={() =>
+                      props.panTo({
+                        lat: Number(location.latitude),
+                        lng: Number(location.longitude),
+                      })
+                    }
+                  >
+                    {location.location_name}
+                  </h4>
+                )}
+              </div>
+            )
+          }
+        })
+      )
+    }
+  }
+
   return (
     <div className={`render ${namer}`}>
       <div className="clicker" onClick={() => combiner()}></div>
-      {render.map((location) => {
+      {renderList()}
+      {/* {render.map((location) => {
         if(location.latitude <= props.north && location.latitude >= props.south && location.longitude >= props.west && location.longitude <= props.east){
           return (
             <div className="location-info" key={location.location_id}>
@@ -55,7 +83,7 @@ function ConditionalRender(props) {
             </div>
           )
         }
-      })}
+      })} */}
     </div>
   )
 }
